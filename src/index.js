a68dabc6a5b80cbb2996ff64b4b4681e16458521
Omilari Mason <omilariamason@gmail.com>
2021-07-02 15:09:05 -0400

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; //import index.css after bootstrap so my css adjustments can overwrite bootstrap
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
