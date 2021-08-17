c187db98f0d42e11318d268ebc4c71b0edd0004b
Omilari Mason <omilariamason@gmail.com>
2021-08-17 16:12:23 -0400

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import { Component } from 'react';
+import DishDetail from './components/DishdetailComponent';
 
 
 
@@ -19,7 +19,7 @@ class App extends Component {
   
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
